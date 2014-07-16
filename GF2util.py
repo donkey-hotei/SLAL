e11b4ed3a6976f620655f3223835323133e0c273
dynsne <t0piary@lavabit.com>
2014-07-16 11:09:12 -0700

    Update GF2util.py

--- GF2util.py
+++ GF2util.py
@@ -1,19 +1,32 @@
 """
-SLAL: Matrix/Vector Utility Functions
+SLAL: Utility functions for working over Galois Field 2
 """
+from random import choice
+from itertools import combinations, chain
+
 from vector import Vector
 from matrix import Matrix
 from GF2 import zero, one
 
-from vectorutil import linear_comb
-# constructing the span of a set of vectors over GF2
-# NOT FINISHED? If you can find a solution... will you?
-def GF2_span(veclist):
+
+def GF2_span(veclist:list)->set:
     """ Returns the set of all linear combinations of the vectors in vectorset."""
+    possible_coeffs = powerset
+    for coeffs in possible_coeffs:
     
-    for 
-        span = { veclist[d] * veclist[d+1] for vector in veclist }
-    return span
-    
-def linear_comb(clist, vlist):
-    
+def linear_comb(clist:list, vlist:list)->Vector:
+    """
+    Returns a linear combination of the vectors in vlist
+    with the corresponding coefficients in clist.
+    """
+    assert len(clist) == len(vlist)
+    return sum([ coeff * vec for (coeff, vec) in zip(clist, vlist) ])
+
+def powerset(coeffs:set)->list:
+    """
+    Gives all the possible n-length subset combinations of a set.
+    """
+    powerset = [ ]
+    for z in chain.from_iterable(combinations(coeffs, r) for r in range(len(coeffs)+1)):
+        powerset.append(z)
+    return powerset
