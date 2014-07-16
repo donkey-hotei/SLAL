285430366b8c55618bb4470acbce2110192213dc
dynsne <t0piary@lavabit.com>
2014-07-16 10:06:10 -0700

    Update GF2util.py

--- GF2util.py
+++ GF2util.py
@@ -8,11 +8,12 @@ from GF2 import zero, one
 from vectorutil import linear_comb
 # constructing the span of a set of vectors over GF2
 # NOT FINISHED? If you can find a solution... will you?
-def GF2_span(labels, veclist):
+def GF2_span(veclist):
     """ Returns the set of all linear combinations of the vectors in vectorset."""
-    span = {}
-    for d in labels:
+    
+    for 
         span = { veclist[d] * veclist[d+1] for vector in veclist }
     return span
     
-# This is still wrong.
+def linear_comb(clist, vlist):
+    
