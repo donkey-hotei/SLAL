c65dbdce2b650f6f2f461bfdfefa9a2a1bff1b83
donkey-hotei <topiary@openmailbox.org>
2015-08-26 22:17:25 -0700

    take out previous rref for the much preferred row_reduce

--- util/echelon.py
+++ util/echelon.py
@@ -1,38 +1,7 @@
 """
-Row Reduce Echelon Form
-
-thanks to http://elonen.iki.fi/code/misc-notes/python-gaussj/
-for the simple algorithm used in the guass_jordan function
+Row Reduction of a Matrix (represented as a list of row Vectors)
 """
-from matrixutil import matrix2rowdict, identity, coldict2matrix
-
-# First Attempts at implementing row reduction
-
-
-def rref(A):
-    """
-    Input: A -> a Matrix
-    Output: echelon_A -> the Matrix A put into echelon form
-    """
-    rowlist = matrix2rowdict(A)  # matrix as row vectors
-    M_rowlist = identity(A.D[1], 1)  # same sq. domain as A
-    column_label_list = sorted(rowlist[0].D, key=str)  # decide on an ordering
-    new_M_rowlist = []
-    rows_left = set(range(len(rowlist)))
-    for c in column_label_list:
-        rows_with_nonzero = [r for r in rows_left if rowlist[r][c] != 0]
-        if rows_with_nonzero != []:  # if there is a pivot point
-            pivot = rows_with_nonzero[0]  # pivot at the first nonzero row
-            rows_left.remove(pivot)
-            new_M_rowlist.append(M_rowlist[pivot])
-            for r in rows_with_nonzero[1:]:
-                multiplier = rowlist[r][c] / rowlist[pivot][c]
-                rowlist[r] -= multiplier * rowlist[pivot]
-                M_rowlist[r] -= multiplier * M_rowlist[pivot]
-        for r in rows_left:
-            new_M_rowlist.append(M_rowlist[r])
-    echelon_A = coldict2matrix(new_M_rowlist)
-    return echelon_A
+from matrixutil import matrix2rowdict
 
 
 def row_reduce(rowlist):
@@ -43,32 +12,18 @@ def row_reduce(rowlist):
     """
     col_label_list = sorted(  # decide on an ordering
         rowlist[0].D, key=str)
-    # we'll represent a matrix as a list of row vectors
-    # initialiing a set of vectors not yet transformed
     rows_left = set(range(len(rowlist)))
-    new_rowlist = []
-    # the algorithm will iterate thru the column labels in order.
+    echelon_rowlist = []
     for c in col_label_list:
-        # among the rows left, list of row-lavels whose rows have a nonzero
-        # position at c
         rows_with_nonzero = [r for r in rows_left if rowlist[r][c] != 0]
         if rows_with_nonzero != []:
-            pivot = rows_with_nonzero[0]  # selects one of the rows
+            pivot = rows_with_nonzero[0]
             rows_left.remove(pivot)
-            # and adds it to the new rowlist
-            new_rowlist.append(rowlist[pivot])
+            echelon_rowlist.append(rowlist[pivot])
             for r in rows_with_nonzero[1:]:
                 rowlist[r] -= \
                     (rowlist[r][c] / rowlist[pivot][c]) * rowlist[pivot]
-    return new_rowlist
-
-
-def guass_jordan(m, epsilon=1.0 / (10**10)):
-    """
-    Puts the matrix into the Reduced Row Echelon Form.
-    Returns True if sucessful, False if the matrix is singular.
-    """
-    return NotImplementedError
+    return echelon_rowlist
 
 
 if __name__ == '__main__':
